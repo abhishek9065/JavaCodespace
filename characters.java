b7fe785693b174855a1071c3b822c83013313a6e
Abhishek kumar <85061211+abhishek9065@users.noreply.github.com>
2025-05-05 12:47:43 +0000

    Basics of java programming

---
+++ characters.java
@@ -0,0 +1,13 @@
+public class characters { 
+        public static void main(String[] args) 
+        {
+        // character only accepts single character
+        // char a = 'ab'; // error
+        char a = 56;   
+        System.out.println(a);
+        
+
+  }
+
+
+}
